330c303396c69db06d393c8b308c2f41bd45b31e
Christopher Cofie Kuzagbe <46811577+Kuzagbe@users.noreply.github.com>
2020-05-09 03:54:25 +0200

    Update index.php

--- Online-Food-Order/index.php
+++ Online-Food-Order/index.php
@@ -11,6 +11,8 @@ session_start();
   <link rel="stylesheet" type = "text/css" href ="css/bootstrap.min.css">
 
   <link rel="stylesheet" type = "text/css" href ="css/index.css">
+  
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.5.1/jquery.min.js" integrity="sha256-9/aliU8dGd2tb6OSsuzixeV4y/faTqgFtohetphbbj0=" crossorigin="anonymous"></script>
 
   <body>
 
